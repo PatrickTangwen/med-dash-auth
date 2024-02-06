30ab8a2e82f107eba54750a43bb2b713320c8b7f
PatrickTangwen <102566928+PatrickTangwen@users.noreply.github.com>
2024-02-05 16:16:27 -0800

    add forms

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,49 @@
-const Login=() =>{
-    return <div>this is Login</div>
-}
+import { Card } from "antd";
+import { Form, Input, Button, Checkbox } from "antd";
+import logo from "@/assets/dashboard_icon.svg";
+import "./index.scss";
 
-export default Login
+const Login = () => {
+    const onFinish = (values)=>{
+        console.log(values)
+    }
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */} 
+        <Form onFinish={onFinish} validateTrigger="onBlur">
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please provide your username!",
+              },
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please provide your password!",
+              },
+            ]}
+          >
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
