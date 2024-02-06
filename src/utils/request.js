0dc4909af08f543744a66450a58c09ff25965685
PatrickTangwen <102566928+PatrickTangwen@users.noreply.github.com>
2024-02-05 16:36:12 -0800

    set up request

---
+++ src/utils/request.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+
+// base URL set up
+const request = axios.create({
+  baseURL: 'http://geek.itheima.net/v1_0',
+  timeout: 5000
+})
+
+
+/*
+Add request interceptors to intercept requests before they are sent, 
+allowing for the insertion of custom configurations.
+*/
+
+request.interceptors.request.use((config)=> {
+    return config
+  }, (error)=> {
+    return Promise.reject(error)
+})
+
+// processing returned data
+request.interceptors.response.use((response)=> {
+    return response
+  }, (error)=> {
+    return Promise.reject(error)
+})
+
+export { request }
