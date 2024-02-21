04469a8e5e500fcf6d56980c4eafed978f7d12cb
PatrickTangwen <102566928+PatrickTangwen@users.noreply.github.com>
2024-02-20 17:15:27 -0800

    modified etl pipeline

--- vital_data_etl/run.py
+++ vital_data_etl/run.py
@@ -1,10 +1,9 @@
-# run.py
 import json
-from vital_data_etl import get_vital_data, save_to_csv
+from etl import get_vital_data, save_to_csv
 
 def main():
     print("program is running")
-    with open('etl.json') as json_file:
+    with open('config.json') as json_file:
         config = json.load(json_file)
     
     auth_key = config['auth_key']
@@ -13,8 +12,9 @@ def main():
     start_date = config['start_date']
     end_date = config['end_date']
     directory = config['directory']
-    
     data = get_vital_data(auth_key, user_id, functions, start_date, end_date)
+    print(f"data is updated!") 
     save_to_csv(data, directory)
+    
 if __name__ == "__main__":
     main()
